852107b9fa87941c6291669c7ebfa8d156b81af6
spaceman007 <skyhe89@outlook.com>
2017-09-19 18:31:15 +0800

    添加几个button

--- src/draw/MyDraw.java
+++ src/draw/MyDraw.java
@@ -1,6 +1,9 @@
 package draw;
 
+import java.awt.BorderLayout;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
 
 public class MyDraw {
 
@@ -12,12 +15,20 @@ public class MyDraw {
     MyDraw o = new MyDraw();
     o.showWelcome();
 
+    JPanel panel = new JPanel();
+    panel.add(new JButton("Venus"));
+    panel.add(new JButton("Jupiter"));
+    panel.add(new JButton("Mars"));
+    panel.add(new JButton("Jupiter"));
+
     JFrame frame = new JFrame();
+    frame.add(panel, BorderLayout.CENTER);
+    frame.add(new JButton("Pluto"), BorderLayout.SOUTH);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setTitle("练习使用JFrame");
     frame.pack();
     frame.setVisible(true);
-    frame.repaint();
+    //frame.repaint();
   }
 
 }

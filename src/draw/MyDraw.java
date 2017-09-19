959d818f7c99dbd67d67d77e228aec8927528cb9
spaceman007 <skyhe89@outlook.com>
2017-09-19 18:11:19 +0800

    初始化项目

---
+++ src/draw/MyDraw.java
@@ -0,0 +1,16 @@
+package draw;
+
+import javax.swing.JFrame;
+
+public class MyDraw {
+
+  public void showWelcome() {
+    System.out.println("欢迎来到这里！");
+  }
+
+  public static void main(String[] args) {
+    MyDraw o = new MyDraw();
+    o.showWelcome();
+  }
+
+}
